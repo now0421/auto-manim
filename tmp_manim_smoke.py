bc76f38f8d8f31e7322f5c2691a998bd0114b4e5
now0421 <sa24225218@mail.ustc.edu.cn>
2026-05-08 17:28:28 +0800

    delete

--- tmp_manim_smoke.py
+++
@@ -1,7 +0,0 @@
-from manim import *
-
-class Smoke(Scene):
-    def construct(self):
-        t = Text("smoke")
-        self.add(t)
-        self.wait(0.1)
--- tmp_manim_tex.py
+++
@@ -1,7 +0,0 @@
-from manim import *
-
-class SmokeTex(Scene):
-    def construct(self):
-        eq = MathTex(r"|x-1|+|x-3|=2")
-        self.add(eq)
-        self.wait(0.1)
